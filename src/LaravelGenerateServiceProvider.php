18286e6eeb690344b47f0dc58b5f531563ae768e
Tan Nguyen <tannp27@gmail.com>
2023-02-25 15:52:25 +0700

    create provider

---
+++ src/LaravelGenerateServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TanHongIT\LaravelGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGenerateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
