20b95abaa515283e7fcc2b333af84bb7ac7d38e4
Tan Nguyen <tannp27@gmail.com>
2023-02-26 22:36:26 +0700

    create config repository

---
+++ src/Repositories/ConfigRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace TanHongIT\LaravelGenerator\Repositories;
+
+use TanHongIT\LaravelGenerator\Exceptions\LaravelGeneratorException;
+
+class ConfigRepository
+{
+    /**
+     * @param string|null $generatorName
+     *
+     * @return array
+     * @throws LaravelGeneratorException
+     */
+    public function generatorConfig(?string $generatorName = null): array
+    {
+        if ($generatorName === null) {
+            $generatorName = config('laravel-generator.default');
+        }
+
+        $defaults = config('laravel-generator.defaults', []);
+        $generators = config('laravel-generator.generators', []);
+
+        if (! isset($generators[$generatorName])) {
+            throw new LaravelGeneratorException('Generator name not found');
+        }
+
+        return $this->mergeConfig($defaults, $generators[$generatorName]);
+    }
+
+    /**
+     * @param array $defaults
+     * @param array $generatorName
+     *
+     * @return array
+     */
+    private function mergeConfig(array $defaults, array $generatorName): array
+    {
+        $merged = $defaults;
+
+        foreach ($generatorName as $key => &$value) {
+            if (isset($defaults[$key])
+                && $this->isAssociativeArray($defaults[$key])
+                && $this->isAssociativeArray($value)
+            ) {
+                $merged[$key] = $this->mergeConfig($defaults[$key], $value);
+                continue;
+            }
+
+            $merged[$key] = $value;
+        }
+
+        return $merged;
+    }
+
+    /**
+     * @param mixed $key
+     *
+     * @return bool
+     */
+    private function isAssociativeArray(mixed $key): bool
+    {
+        return is_array($key) && count(array_filter(array_keys($key), 'is_string')) > 0;
+    }
+}
