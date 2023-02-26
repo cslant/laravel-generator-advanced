de4b0196a667b4b9250253ee0ebbd75ffc81b0bc
Tan Nguyen <tannp27@gmail.com>
2023-02-26 22:36:26 +0700

    create config repository

---
+++ src/Exceptions/LaravelGeneratorException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TanHongIT\LaravelGenerator\Exceptions;
+
+use Exception;
+
+class LaravelGeneratorException extends Exception
+{
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
+     */
+    public function __construct(string $message = '', int $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
