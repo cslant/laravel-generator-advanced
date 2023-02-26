829610aebd2db574cdb0a81e6b7f82da5d821d5a
Tan Nguyen <tannp27@gmail.com>
2023-02-26 12:25:26 +0700

    fix route and load views

--- src/Providers/LaravelGenerateServiceProvider.php
+++ src/Providers/LaravelGenerateServiceProvider.php
@@ -7,6 +7,29 @@ use Illuminate\Support\ServiceProvider;
 class LaravelGenerateServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $viewPath = __DIR__ . '/../../resources/views';
+        $this->loadViewsFrom($viewPath, 'laravel-generator');
+
+        // Publish a config file
+        $configPath = __DIR__ . '/../../config/laravel-generator.php';
+        $this->publishes([
+            $configPath => config_path('laravel-generator.php'),
+        ], 'config');
+
+        // Include routes
+        $routePath = __DIR__ . '/../../routes/web.php';
+        if (file_exists($routePath)) {
+            $this->loadRoutesFrom($routePath);
+        }
+    }
+
+    /**
      * Register services.
      *
      * @return void
@@ -14,21 +37,5 @@ class LaravelGenerateServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $viewPath = __DIR__ . '/../resources/views';
-
-        // Include routes
-        $routePath = __DIR__ . '/../routes/web.php';
-        if (file_exists($routePath)) {
-            $this->loadRoutesFrom($routePath);
-        }
     }
 }
