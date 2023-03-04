b2712be4b2a03f1ab8c46f6c52095127a4654231
Tan Nguyen <tannp27@gmail.com>
2023-03-04 13:15:31 +0700

    update provider

--- src/Providers/LaravelGenerateServiceProvider.php
+++ src/Providers/LaravelGenerateServiceProvider.php
@@ -3,6 +3,7 @@
 namespace TanHongIT\LaravelGenerator\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TanHongIT\LaravelGenerator\Helpers\ConfigHelper;
 
 class LaravelGenerateServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,11 @@ class LaravelGenerateServiceProvider extends ServiceProvider
             $configPath => config_path('laravel-generator.php'),
         ], 'config');
 
+        // Publish views
+        $this->publishes([
+            __DIR__ . '/../../resources/views' => config('laravel-generator.defaults.paths.views'),
+        ], 'views');
+
         // Include routes
         $routePath = __DIR__ . '/../../routes/web.php';
         if (file_exists($routePath)) {
@@ -38,5 +44,19 @@ class LaravelGenerateServiceProvider extends ServiceProvider
     {
         $configPath = __DIR__ . '/../../config/laravel-generator.php';
         $this->mergeConfigFrom($configPath, 'laravel-generator');
+
+        $this->app->singleton('laravel-generator', function () {
+            return new ConfigHelper();
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array|null
+     */
+    public function provides(): ?array
+    {
+        return ['laravel-generator'];
     }
 }
