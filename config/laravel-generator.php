214e3e12685391bbe09ca8f81984ad6046e877ad
Tan Nguyen <tannp27@gmail.com>
2023-03-04 13:16:08 +0700

    create helper and update config

--- config/laravel-generator.php
+++ config/laravel-generator.php
@@ -13,5 +13,11 @@ return [
         ],
     ],
 
-    'defaults' => [],
+    'defaults' => [
+        'paths' => [
+            'views' => base_path('resources/views/tool/laravel-generator'),
+
+            'laravel_generator_assets_path' => env('TH_LARAVEL_GENERATOR_ASSETS_PATH', 'vendor/tanhongit/laravel-generator-api/assets'),
+        ],
+    ],
 ];
