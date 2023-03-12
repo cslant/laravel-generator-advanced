758eedeec2edc67210b032a052e8c122b785b44f
Tan Nguyen <tannp27@gmail.com>
2023-03-13 01:02:30 +0700

    add api request abstract class

---
+++ src/Http/Requests/ApiRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TanHongIT\LaravelGenerator\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+/**
+ * Class ApiRequest.
+ * @package TanHongIT\LaravelGenerator\Http\Requests
+ *
+ * This class is used to handle the validation error response. It will return the error message in JSON format.
+ */
+abstract class ApiRequest extends FormRequest
+{
+    /**
+     * @param Validator $validator
+     *
+     * @return void
+     */
+    function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => $validator->errors()->toArray(),
+            ], 403, [], JSON_UNESCAPED_UNICODE)
+        );
+    }
+}
